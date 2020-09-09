2b4ad4e0b8fff3b4f9f80cc904be2e9a4576f3ba
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-09-09 10:46:37 +0700

    update configs

---
+++ src/main/java/com/codegym/hotel/booking/service/impl/MessageServiceImpl.java
@@ -0,0 +1,6 @@
+package com.codegym.hotel.booking.service.impl;
+
+import com.codegym.hotel.booking.service.MessageService;
+
+public class MessageServiceImpl implements MessageService {
+}
---
+++ src/main/java/com/codegym/hotel/booking/service/impl/UserServiceImpl.java
@@ -0,0 +1,6 @@
+package com.codegym.hotel.booking.service.impl;
+
+import com.codegym.hotel.booking.service.UserService;
+
+public class UserServiceImpl implements UserService {
+}
